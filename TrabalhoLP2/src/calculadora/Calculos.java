6f676d42a25d7df78cf04f1d238fcf24fa7d76fb
PauloSergioRezende <40127216+PauloSergioRezende@users.noreply.github.com>
2018-10-27 00:21:03 -0300

    JooJ
    Calculadora semi pronta

---
+++ TrabalhoLP2/src/calculadora/Calculos.java
@@ -0,0 +1,306 @@
+package calculadora;
+
+import java.util.Collections;
+
+public class Calculos {
+
+	private static StringBuffer operando = new StringBuffer();
+	private static StringBuffer expressao = new StringBuffer();
+	private static String operacao;
+	private static String digito;
+	private static String resultado;
+
+	// limpa variáveis
+	public static void clean() {
+		operando = new StringBuffer();
+		expressao = new StringBuffer();
+		operacao = "";
+		digito = "";
+		resultado = "";
+	}
+
+	// coleta operando
+	public static void coletaOperando() {
+		operando.append(digito);
+		expressao.append(digito);
+	}
+
+	public static void coletaOperando(String key) {
+		setDigito(key);
+		coletaOperando();
+	}
+
+	// monta expressão
+	public static void montaExpressao() {
+		expressao.append(operacao);
+		operando = new StringBuffer("");
+	}
+
+	// monta expressão
+	public static void montaExpressao(String key) {
+		setOperacao(key);
+		montaExpressao();
+	}
+
+	public static void efetuaCalculo() {
+		System.out.println("-----> INICIO: efetuaCalculo <-----");
+		resultado = calculaExprecao(expressao).toString();
+		System.out.println("-----> FIM: efetuaCalculo <-----");
+	}
+
+	// Getters & setters
+	public static StringBuffer getOperando() {
+		return operando;
+	}
+
+	public static void setOperando(StringBuffer operando) {
+		Calculos.operando = operando;
+	}
+
+	public static StringBuffer getExpressao() {
+		return expressao;
+	}
+
+	public static void setExpressao(StringBuffer expressao) {
+		Calculos.expressao = expressao;
+	}
+
+	public static String getOperacao() {
+		return operacao;
+	}
+
+	public static void setOperacao(String operacao) {
+		Calculos.operacao = operacao;
+	}
+
+	public static String getDigito() {
+		return digito;
+	}
+
+	public static void setDigito(String digito) {
+		Calculos.digito = digito;
+	}
+
+	public static String getResultado() {
+		return resultado;
+	}
+
+	public static void setResultado(Double resultado) {
+		Calculos.resultado = String.valueOf(resultado);
+	}
+
+	public static StringBuffer calculaExprecao(StringBuffer key) {
+		int aux;
+
+		aux = key.indexOf("(");
+		if (aux != -1) {
+			System.out.println("parenteses encontrado");
+			System.out.println("Estado da expressão pré calculo:" + key);
+			key = subExprecao(key, aux);
+			System.out.println("Estado da expressão pos calculo:" + key);
+		}
+		
+		aux = key.indexOf(")");
+		if (aux != -1) {
+			return new StringBuffer("!!Expreção invalida!!");
+		}
+		
+		aux = key.indexOf("r");
+		if (aux != -1) {
+			System.out.println("Exponenciação encontrado");
+			System.out.println("Estado da expressão pré calculo:" + key);
+			key = calcula(key, aux, "r");
+			System.out.println("Estado da expressão pos calculo:" + key);
+		}
+		
+		aux = key.indexOf("^");
+		if (aux != -1) {
+			System.out.println("Exponenciação encontrado");
+			System.out.println("Estado da expressão pré calculo:" + key);
+			key = calcula(key, aux, "^");
+			System.out.println("Estado da expressão pos calculo:" + key);
+		}
+		
+		aux = key.indexOf("*");
+		if (aux != -1) {
+			System.out.println("multiplicação encontrado");
+			System.out.println("Estado da expressão pré calculo:" + key);
+			key = calcula(key, aux, "*");
+			System.out.println("Estado da expressão pos calculo:" + key);
+		}
+
+		aux = key.indexOf("/");
+		if (aux != -1) {
+			System.out.println("Divisão encontrado");
+			System.out.println("Estado da expressão pré calculo:" + key);
+			key = calcula(key, aux, "/");
+			System.out.println("Estado da expressão pos calculo:" + key);
+		}
+
+		aux = key.indexOf("+");
+		if (aux != -1) {
+			System.out.println("soma encontrado");
+			System.out.println("Estado da expressão pré calculo:" + key);
+			key = calcula(key, aux, "+");
+			System.out.println("Estado da expressão pos calculo:" + key);
+		}
+
+		aux = key.indexOf("-");
+		if (aux != -1) {
+			System.out.println("subtração encontrado");
+			System.out.println("Estado da expressão pré calculo:" + key);
+			key = calcula(key, aux, "-");
+			System.out.println("Estado da expressão pos calculo:" + key);
+		}
+
+		return key;
+	}
+
+	private static StringBuffer calcula(StringBuffer key, int aux, String operador) {
+		
+			int aux2, aux3;
+			Double v1, v2;
+
+			aux2 = key.indexOf(operador, aux + 1);
+			if (aux2 != -1) {
+				key = calcula(key, aux2, operador);
+			}
+
+			if (operador.equals("+")) {
+				aux2 = key.length();
+			} else{
+				int temp = indexBordaNumero(key.substring(aux + 1, key.length()));
+				if (temp == 0) {
+					if (((key.toString().charAt(aux + 1) != '+') && (key.toString().charAt(aux + 1) != '-')|| 
+							indexBordaNumero(key.substring(aux+2, key.length())) != key.substring(aux+2, key.length()).length())) {
+						return key = new StringBuffer("!!!Expreção invalida!!!");
+					}
+					temp = indexBordaNumero(key.substring(aux + 2, key.length())+1);
+				}
+				aux2 = aux + temp + 1;
+			}
+
+			if (aux + 1 == aux2) {
+				v1 = 0D;
+			} else {
+				v1 = Double.valueOf(key.substring(aux + 1, aux2));
+			}
+
+			aux3 = lastIndexBordaNumero(key.substring(0, aux));
+			if (aux3 + 1 == aux) {
+				if(operador == "r") {
+					v2 = 2D;
+				}else {
+					v2 = 0D;
+				}
+				
+			} else {
+				v2 = Double.valueOf(key.substring(aux3 + 1, aux));
+			}
+
+			key = new StringBuffer(key.delete(aux3 + 1, aux2));
+			key.insert(aux3 + 1, calculos(v1, v2, operador));
+			return key;
+	}
+
+	private static String calculos(Double v1, Double v2, String operador) {
+		switch (operador) {
+		case "+":
+			return String.valueOf(v2 + v1);
+		case "-":
+			return String.valueOf(v2 - v1);
+		case "*":
+			return String.valueOf(v2 * v1);
+		case "/":
+			return String.valueOf(v2 / v1);
+		case "^":
+			return String.valueOf(Math.pow(v2, v1));
+		case "r":
+			
+			return String.valueOf(Math.pow(v1,(1/v2)));
+		default:
+			return "b";
+		}
+	}
+
+	private static int lastIndexBordaNumero(String substring) {
+		int[] t = new int[7];
+		int r = -1;
+
+		t[0] = substring.lastIndexOf("^");
+		t[1] = substring.lastIndexOf("raiz(");
+		t[2] = substring.lastIndexOf("log");
+		t[3] = substring.lastIndexOf("*");
+		t[4] = substring.lastIndexOf("/");
+		t[5] = substring.lastIndexOf("+");
+		t[6] = substring.lastIndexOf("-");
+
+		for (int i = 1; i <= 6; i++) {
+			if (t[i] != -1) {
+				if (t[i] > r) {
+					r = t[i];
+				}
+			}
+		}
+
+		return r;
+	}
+
+	private static int indexBordaNumero(String substring) {
+		int[] t = new int[7];
+		int r = substring.length();
+
+		t[0] = substring.indexOf("^");
+		t[1] = substring.indexOf("r");
+		t[2] = substring.indexOf("log");
+		t[3] = substring.indexOf("*");
+		t[4] = substring.indexOf("/");
+		t[5] = substring.indexOf("+");
+		t[6] = substring.indexOf("-");
+
+		for (int i = 1; i <= 6; i++) {
+			if (t[i] != -1) {
+
+				if (t[i] < r) {
+					r = t[i];
+				}
+
+			}
+		}
+
+		return r;
+	}
+
+	private static StringBuffer subExprecao(StringBuffer key, int aux) {
+
+		if (aux == key.length() - 1) {
+			return key.deleteCharAt(aux).append("+0");
+		}
+
+		int iaux = key.indexOf("(", aux + 1);
+		if (iaux != -1) {
+			key = subExprecao(key, iaux);
+		} 
+		
+			int aux2 = key.indexOf(")");
+			if (aux2 == -1) {
+				aux2 = key.length();
+			}
+			StringBuffer nKey = new StringBuffer(key.substring(aux + 1, aux2));
+			nKey = calculaExprecao(nKey);
+			key = new StringBuffer(key.delete(aux, aux2 + 1));
+			if(lastIndexBordaNumero(key.substring(0, aux)) != aux-1  ) {
+				if(key.toString().charAt(aux-1) == '(') {
+					key.insert(aux, "+"+ nKey.toString());
+				}else {
+					key.insert(aux, "*"+ nKey.toString());
+				}
+				
+			}else {
+				key.insert(aux, nKey.toString());
+			}
+			
+		return key;
+	}
+
+}
---
+++ TrabalhoLP2/src/calculadora/Sudai.java
@@ -0,0 +1,269 @@
+package calculadora;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.FlowLayout;
+import javax.swing.JTextField;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter.DEFAULT;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.TextField;
+
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import com.jgoodies.forms.layout.RowSpec;
+
+import net.miginfocom.swing.MigLayout;
+
+public class Sudai implements ActionListener, KeyListener {
+
+	private JFrame frame;
+	private JTextField textFieldResult;
+	private JButton Erased;
+	private JButton soma;
+	private Component subtrai;
+	private Component multiplica;
+	private JButton divide;
+	private JButton ponto;
+	private JButton raiz;
+	private JButton um;
+	private JButton dois;
+	private JButton tres;
+	private JButton sete;
+	private JButton quatro;
+	private JButton cinco;
+	private JButton seis;
+	private JButton oito;
+	private JButton zero;
+	private JButton nove;
+	private JPanel panel_principal;
+	private JPanel panel;
+	private JPanel panel_teclado;
+	private JButton elevado;
+	private JButton aParenteses;
+	private JButton fParenteses;
+	private JButton igual;
+	private JTextField textFieldOp;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Sudai window = new Sudai();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Sudai() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setTitle("Tela");
+		frame.setLocationRelativeTo(null);
+		frame.setSize(450, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		panel_principal = new JPanel();
+		frame.getContentPane().add(panel_principal, BorderLayout.CENTER);
+		panel_principal.setLayout(null);
+
+		textFieldResult = new JTextField();
+		textFieldResult.setEditable(false);
+		textFieldResult.setBounds(0, 40, 432, 50);
+		panel_principal.add(textFieldResult);
+		textFieldResult.setColumns(10);
+		
+		textFieldOp = new JTextField();
+		textFieldOp.setEditable(false);
+		textFieldOp.setBounds(0, 0, 432, 40);
+		panel_principal.add(textFieldOp);
+		textFieldOp.setColumns(10);
+
+		panel = new JPanel();
+		panel.setBounds(0, 88, 432, 90);
+		panel_principal.add(panel);
+		panel.setLayout(null);
+
+		Erased = new JButton("Excluir");
+		Erased.setBounds(293, 13, 127, 64);
+		Erased.addActionListener(this);
+		panel.add(Erased);
+
+		panel_teclado = new JPanel();
+		panel_teclado.setBounds(0, 176, 432, 277);
+		panel_principal.add(panel_teclado);
+		panel_teclado.setLayout(new GridLayout(4, 5, 2, 2));
+
+		um = new JButton("1");
+		panel_teclado.add(um);
+
+		dois = new JButton("2");
+		panel_teclado.add(dois);
+
+		tres = new JButton("3");
+		panel_teclado.add(tres);
+
+		quatro = new JButton("4");
+		panel_teclado.add(quatro);
+
+		cinco = new JButton("5");
+		panel_teclado.add(cinco);
+
+		seis = new JButton("6");
+		panel_teclado.add(seis);
+
+		sete = new JButton("7");
+		panel_teclado.add(sete);
+
+		oito = new JButton("8");
+		panel_teclado.add(oito);
+
+		nove = new JButton("9");
+		panel_teclado.add(nove);
+
+		zero = new JButton("0");
+		panel_teclado.add(zero);
+
+		soma = new JButton("+");
+		panel_teclado.add(soma);
+
+		subtrai = new JButton("-");
+		panel_teclado.add(subtrai);
+
+		multiplica = new JButton("*");
+		panel_teclado.add(multiplica);
+
+		divide = new JButton("/");
+		panel_teclado.add(divide);
+
+		ponto = new JButton(".");
+		panel_teclado.add(ponto);
+
+		raiz = new JButton("raiz(");
+		panel_teclado.add(raiz);
+
+		elevado = new JButton("^");
+		panel_teclado.add(elevado);
+
+		aParenteses = new JButton("(");
+		panel_teclado.add(aParenteses);
+
+		fParenteses = new JButton(")");
+		panel_teclado.add(fParenteses);
+
+		igual = new JButton("=");
+		panel_teclado.add(igual);
+
+		for (Component c : panel_teclado.getComponents()) {
+			((JButton) c).addActionListener(this);
+			((JButton) c).addKeyListener(this);
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(((JButton)e.getSource()).getText() == "Excluir") {
+			Calculos.clean();
+			textFieldResult.setText("");
+			textFieldOp.setText("");
+			return;
+		}
+		String key = ((JButton) e.getSource()).getText();
+		tratamentoTeclado(key);
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		String key = String.valueOf(arg0.getKeyChar());
+		tratamentoTeclado(key);
+
+	}
+
+	private void tratamentoTeclado(String key) {
+		switch (key) {
+		case "1":
+		case "2":
+		case "3":
+		case "4":
+		case "5":
+		case "6":
+		case "7":
+		case "8":
+		case "9":
+		case "0":
+		case ".":
+			System.out.println("------->>> Coleta operando <<<------- ");
+			Calculos.coletaOperando(key);
+			System.out.println("Digito: " + Calculos.getDigito());
+			System.out.println("Operando: " + Calculos.getOperando());
+			System.out.println("Expressao: " + Calculos.getExpressao());
+			textFieldResult.setText(Calculos.getOperando().toString());
+			textFieldOp.setText(Calculos.getExpressao().toString());
+			break;
+		case "raiz(":
+			key = "r(";
+		case "+":
+		case "-":
+		case "*":
+		case "^":
+		case ")":
+		case "(":
+		case "/":
+			System.out.println("------->>> Monta expressão <<<------- ");
+			Calculos.montaExpressao(key);
+			System.out.println("Expressão: " + Calculos.getExpressao());
+			textFieldResult.setText(Calculos.getOperacao().toString());
+			textFieldOp.setText(Calculos.getExpressao().toString());
+			break;
+		case "=":
+			System.out.println("------->>> Efetua cálculo <<<------- ");
+			Calculos.efetuaCalculo();
+			textFieldOp.setText(String.valueOf(Calculos.getResultado()));
+			Calculos.clean();
+			break;
+		
+		default:
+			break;
+		}
+	}
+}
